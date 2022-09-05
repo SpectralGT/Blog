9246cfab6d98ca5b7b8ca0347919c501027f8a47
SpectralGT <atharvsingh1396@gmail.com>
2022-09-05 18:22:02 +0530

    created post page styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/admin");
-  eleventyConfig.addPassthroughCopy('src/style.css');
+  eleventyConfig.addPassthroughCopy('src/styles');
   return{
     dir:{
       input: "src",
