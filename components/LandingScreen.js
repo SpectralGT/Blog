101f09c9d15651d473aa8ff363217c81d25a0e03
SpectralGT <atharvsingh1396@gmail.com>
2023-07-05 17:54:58 +0530

    changed colors

---
+++ components/LandingScreen.js
@@ -0,0 +1,10 @@
+import "../styles/LandingScreen.css"
+
+export default function LandingScreen(){
+    return(
+        <div className="landingScreen">
+            <span className="text">Atharv
+            Singh</span>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,59 @@
-import fs from 'fs';
-import matter from 'gray-matter';
-import Image from 'next/image';
-import Link from 'next/link';
+import fs from "fs";
+import matter from "gray-matter";
+import Image from "next/image";
+import Link from "next/link";
+import LandingScreen from "components/LandingScreen";
 
 // The Blog Page Content
-export default function Index({posts}){
-    return <main>
-        {posts.map(post => {
-            //extract slug and frontmatter
-            const {slug, frontmatter} = post
-            //extract frontmatter properties
-            const {title, author, category, date, bannerImage, tags} = frontmatter
+export default function Index({ posts }) {
+  return (
+    <>
+      <LandingScreen />
+      <main>
+        {posts.map((post) => {
+          //extract slug and frontmatter
+          const { slug, frontmatter } = post;
+          //extract frontmatter properties
+          const { title, author, category, date, bannerImage, tags } =
+            frontmatter;
 
-            //JSX for individual blog listing
-            return <article key={title}>
-                <Link href={`/posts/${slug}`}>
-                    <h1>{title}</h1>
-                </Link>
-                <h3>{author}</h3>
-                <h3>{date}</h3>
+          //JSX for individual blog listing
+          return (
+            <article key={title}>
+              <Link href={`/posts/${slug}`}>
+                <h1>{title}</h1>
+              </Link>
+              <h3>{tags}</h3>
+              <h3>{date}</h3>
+              <h3 className="category">{category}</h3>
             </article>
+          );
         })}
-    </main>
+      </main>
+    </>
+  );
 }
 
-
 //Generating the Static Props for the Blog Page
-export async function getStaticProps(){
-    // get list of files from the posts folder
-    const files = fs.readdirSync('posts');
-    // get frontmatter & slug from each post
-    const posts = files.map((fileName) => {
-        const slug = fileName.replace('.md', '');
-        const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
-        const { data: frontmatter } = matter(readFile);
+export async function getStaticProps() {
+  // get list of files from the posts folder
+  const files = fs.readdirSync("posts");
+  // get frontmatter & slug from each post
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace(".md", "");
+    const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8");
+    const { data: frontmatter } = matter(readFile);
 
-        return {
-          slug,
-          frontmatter,
-        };
-    });
-
-    // Return the pages static props
     return {
-        props: {
-          posts,
-        },
+      slug,
+      frontmatter,
     };
+  });
+
+  // Return the pages static props
+  return {
+    props: {
+      posts,
+    },
+  };
 }
